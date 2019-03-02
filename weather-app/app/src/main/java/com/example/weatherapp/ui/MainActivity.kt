4eeecdb5b477eed88f686ad94fe9a1f83028270f
Diego Pizzo <pizzodi@gmail.com>
2019-03-02 14:09:41 +0000

    start fragment from oncreate main activity

--- weather-app/app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
+++ weather-app/app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
@@ -3,11 +3,21 @@ package com.example.weatherapp.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weatherapp.R
+import com.example.weatherapp.ui.weatherfragment.WeatherFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fragmentManager = supportFragmentManager
+        val fragment = fragmentManager.findFragmentByTag(WeatherFragment.TAG_WEATHER_FRAGMENT)
+        if (fragment == null) {
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragment_container,
+                WeatherFragment.newInstance(null),
+                WeatherFragment.TAG_WEATHER_FRAGMENT
+            ).commit()
+        }
     }
 }
