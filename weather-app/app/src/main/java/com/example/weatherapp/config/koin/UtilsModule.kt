b6dd81d41a036c5d38cea2f8284640c142106c9f
Diego Pizzo <pizzodi@gmail.com>
2019-03-04 15:33:41 +0000

    added in the viewmodel the business logic to retrieve the data

--- weather-app/app/src/main/java/com/example/weatherapp/config/koin/UtilsModule.kt
+++ weather-app/app/src/main/java/com/example/weatherapp/config/koin/UtilsModule.kt
@@ -1,9 +1,13 @@
 package com.example.weatherapp.config.koin
 
+import android.content.Context
+import android.net.ConnectivityManager
 import com.example.weatherapp.business.creator.WeatherDataViewModelCreator
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module.module
 
 val utilsModule = module {
 
     single { WeatherDataViewModelCreator() }
+    single { androidApplication().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager }
 }
--- weather-app/app/src/main/java/com/example/weatherapp/ui/weatherfragment/WeatherViewModel.kt
+++ weather-app/app/src/main/java/com/example/weatherapp/ui/weatherfragment/WeatherViewModel.kt
@@ -1,32 +1,66 @@
 package com.example.weatherapp.ui.weatherfragment
 
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.weatherapp.business.dataviewmodel.WeatherDataViewModel
+import com.example.weatherapp.business.db.repository.WeatherRepository
 import com.example.weatherapp.business.interactor.Interactor
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
 class WeatherViewModel(
-    private val interactor: Interactor
+    private val interactor: Interactor,
+    private val repository: WeatherRepository,
+    private val connectivityManager: ConnectivityManager
 ) : ViewModel() {
 
-    private lateinit var disposable: Disposable
+    private lateinit var disposableDB: Disposable
+    private lateinit var disposableNetwork: Disposable
     val weatherMutableLiveData: MutableLiveData<WeatherDataViewModel> = MutableLiveData()
     val progressBarVisibility: MutableLiveData<Int> = MutableLiveData()
     val errorMessage: MutableLiveData<String> = MutableLiveData()
 
     fun getWeatherData(latitude: Double, longitude: Double) {
+        if (checkIfTheUserIsOnline()) {
+            getWeatherDataFromNetwork(latitude, longitude)
+        } else {
+            getWeatherDataFromDB()
+        }
+    }
+
+    private fun getWeatherDataFromNetwork(latitude: Double, longitude: Double) {
         val single = interactor.getWeather(latitude, longitude)
-        disposable = single.subscribeOn(Schedulers.io())
+        disposableNetwork = single.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .doOnSubscribe { onStart() }
             .doFinally { onEnd() }
             .subscribe(
-                { t1 -> onSuccess(t1) },
+                { weatherData -> onSuccessNetwork(weatherData) },
                 { onError() })
+    }
+
+    private fun getWeatherDataFromDB() {
+        val single = repository.loadWeather()
+        disposableDB = single.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { onStart() }
+            .doFinally { onEnd() }
+            .subscribe(
+                { weatherData -> onSuccessDB(weatherData) },
+                { onError() })
+    }
+
+    private fun saveWeatherOnDB(weatherDataViewModel: WeatherDataViewModel) {
+        repository.insertWeather(weatherDataViewModel)
+    }
+
+    private fun checkIfTheUserIsOnline(): Boolean {
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        return activeNetwork?.isConnected == true
     }
 
     private fun onStart() {
@@ -38,8 +72,13 @@ class WeatherViewModel(
         progressBarVisibility.value = View.GONE
     }
 
-    private fun onSuccess(weatherDataViewModel: WeatherDataViewModel) {
+    private fun onSuccessNetwork(weatherDataViewModel: WeatherDataViewModel) {
         weatherMutableLiveData.value = weatherDataViewModel
+        saveWeatherOnDB(weatherDataViewModel)
+    }
+
+    private fun onSuccessDB(weatherDataViewModel: WeatherDataViewModel) {
+        if (weatherDataViewModel.isExpired == false) weatherMutableLiveData.value = weatherDataViewModel else onError()
     }
 
     private fun onError() {
@@ -48,6 +87,7 @@ class WeatherViewModel(
 
     override fun onCleared() {
         super.onCleared()
-        disposable.dispose()
+        disposableNetwork.dispose()
+        disposableDB.dispose()
     }
 }
