db3639fec4f64f29d8e7781c9533ec633eeb47c3
Diego Pizzo <pizzodi@gmail.com>
2019-03-04 15:30:09 +0000

    converter test

---
+++ weather-app/app/src/test/java/com/example/weatherapp/business/db/converter/TimestampConverterTest.kt
@@ -0,0 +1,32 @@
+package com.example.weatherapp.business.db.converter
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.threeten.bp.ZoneId
+import org.threeten.bp.ZonedDateTime
+
+@RunWith(JUnit4::class)
+class TimestampConverterTest {
+
+    @Test
+    fun toZonedDateTimeTest() {
+        val zoneDateTime =
+            TimestampConverter.toZonedDateTime("2019-03-04T10:39:37.640Z[UTC]")//dd/MM/yyyy HH:mm-> 04/03/2019 10:39
+        assertEquals(zoneDateTime!!.dayOfMonth, 4)
+        assertEquals(zoneDateTime.month.value, 3)
+        assertEquals(zoneDateTime.year, 2019)
+        assertEquals(zoneDateTime.hour, 10)
+        assertEquals(zoneDateTime.minute, 39)
+        assertEquals(zoneDateTime.second, 37)
+        assertEquals(zoneDateTime.zone, ZoneId.of("UTC"))
+    }
+
+    @Test
+    fun fromZonedDateTime() {
+        val zoneDateTimeString =
+            TimestampConverter.fromZonedDateTime(ZonedDateTime.of(1990, 1, 17, 12, 13, 14, 0, ZoneId.of("UTC")))
+        assertEquals(zoneDateTimeString, "1990-01-17T12:13:14Z[UTC]")
+    }
+}
