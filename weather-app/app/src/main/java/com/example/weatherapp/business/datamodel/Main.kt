e9e0aad688c01b4467324d14803312d6899c0f64
Diego Pizzo <pizzodi@gmail.com>
2019-03-04 09:17:56 +0000

    fix data models

--- weather-app/app/src/main/java/com/example/weatherapp/business/datamodel/Main.kt
+++ weather-app/app/src/main/java/com/example/weatherapp/business/datamodel/Main.kt
@@ -2,4 +2,4 @@ package com.example.weatherapp.business.datamodel
 
 import com.google.gson.annotations.Expose
 
-data class Main(@Expose val temp: Int)
+data class Main(@Expose val temp: Double)
--- weather-app/app/src/main/java/com/example/weatherapp/business/datamodel/Wind.kt
+++ weather-app/app/src/main/java/com/example/weatherapp/business/datamodel/Wind.kt
@@ -2,4 +2,4 @@ package com.example.weatherapp.business.datamodel
 
 import com.google.gson.annotations.Expose
 
-data class Wind(@Expose val speed: Int, @Expose val deg: Int)
+data class Wind(@Expose val speed: Double, @Expose val deg: Double)
